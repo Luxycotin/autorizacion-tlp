60db0dcb7a4e189c4fca07ee7141e36cfb97eebc
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-09-13 12:39:27 -0300

    Se comenzó con tarea

--- backend/src/controllers/todos.controllers.js
+++ backend/src/controllers/todos.controllers.js
@@ -1,7 +1,9 @@
 import { database } from "../db/database.js";
 
-export const getAllTodosCtrl = (req, res) => {
-  const todos = database.todos;
 
+export const getAllTodosCtrl = (req, res) => {
+  const todos = database.todos.filter((todo)=>todo.owner === req.user.id)
+  req.user.id;
   res.json({ todos });
 };
+
